163d55c24c9104c2aa47d373cc0df4f919fc7b6c
hendriq <voni.henrique@gmail.com>
2018-12-13 13:58:30 -0200

    gulp js scripts

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,20 @@ const gulp = require('gulp');
 const browserify = require('browserify');
 const browserSync = require('browser-sync');
 const sass = require('gulp-sass');
+const babelify = require('babelify');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+
 
 gulp.task('serve', () => {
 
   browserSync.init({
-    server: './'
+    server: 'dist'
   });
 
-  gulp.watch('src/sass/*.scss', gulp.series('style'));
-  gulp.watch('*.html').on('change', browserSync.reload);
+  gulp.watch('src/sass/**/*.scss', gulp.series('style'));
+  gulp.watch('src/js/**/*.js', gulp.series('js'));
+  gulp.watch('dist/*.html').on('change', browserSync.reload);
 
 });
 
@@ -20,5 +25,22 @@ gulp.task('style', () => {
           .pipe(gulp.dest('./dist/css/'))
           .pipe(browserSync.stream());
 });
+
+// Don't forget to add JS scripts here as you create new ones.
+const scriptsSRC = ['src/js/index.js'];
+
+gulp.task('js', () => {
+  return browserify({
+    entries: scriptsSRC,
+    debug: true,
+  })
+    .transform(babelify, { presets: ['@babel/preset-env'] })
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(gulp.dest('./dist/js'))
+    .pipe(browserSync.stream());
+});
+
 
 gulp.task('default', gulp.series('serve'));
